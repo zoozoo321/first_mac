d51099b9c59bddfb0d5a2a4ff1eaadf95e734f41
조민형 <danielcho@jominhyeong-ui-MacBookPro.local>
2024-01-17 20:43:48 +0900

    Initial commit

---
+++ src/main/java/dev/mhcho/in/mac/first_mac/FirstMacApplication.java
@@ -0,0 +1,13 @@
+package dev.mhcho.in.mac.first_mac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mhcho/in/mac/first_mac/FirstMacApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mhcho.in.mac.first_mac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
